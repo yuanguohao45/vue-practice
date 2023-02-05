8c52dd40574433cc167331f3b4420bc4e501c798
yuanguohao <yuanguohao43@163.com>
2023-02-05 23:57:06 +0800

    cypress 测试

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -4,5 +4,9 @@ describe('My First Test', () => {
   it('visits the app root url', () => {
     cy.visit('/')
     cy.contains('h1', 'You did it!')
+    cy.viewport('macbook-15')
+    cy.viewport('macbook-13')
+    cy.viewport('ipad-2')
+    cy.viewport('iphone-6+')
   })
 })
